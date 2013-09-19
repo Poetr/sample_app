1bcd44c0b7c6f445b24081f361403f79c5c198a5
Peter Broers <P.Broers@outlook.com>
2013-09-19 21:51:10 +0200

    Finish sign in

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -28,6 +28,14 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      #  it { should have_selector('div.alert.alert-error', text: 'Welcome') }
+      end
     end
 
     describe "with valid information" do
@@ -37,10 +45,24 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
+      
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
+      end    
     end
   end
 end
